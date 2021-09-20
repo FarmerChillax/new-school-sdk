802d740c9098cc72f9a35f856c43a75a7dd2932d
Farmer.Chillax <farmer-chong@qq.com>
2021-09-20 13:05:21 +0800

    remove: remove pypi release msg

--- build/lib/school_sdk/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-    :file: __init__.py
-    :author: -Farmer
-    :url: https://blog.farmer233.top
-    :date: 2021/09/02 19:18:47
-'''
-
-from .client import SchoolClient
--- build/lib/school_sdk/config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-    :file: config.py
-    :author: -Farmer
-    :url: https://blog.farmer233.top
-    :date: 2021/09/04 23:39:20
-'''
