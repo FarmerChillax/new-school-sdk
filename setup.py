178163792a0c0305ba2ace136579a9aba083fd2f
FarmerChillax <farmer-chong@qq.com>
2023-02-25 15:39:15 +0800

    release v1.4.10

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(basedir, "README.md"), encoding='utf-8') as f:
 setup(
     name="school-sdk",
     author="farmer.chillax",
-    version="1.4.9",
+    version="1.4.10",
     license='MIT',
     author_email="farmer-chong@qq.com",
     description="zf School SDK for Python",
